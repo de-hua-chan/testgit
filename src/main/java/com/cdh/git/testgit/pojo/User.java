e96075404ebbd7ccea6f594686e893f4dfc53b5a
chen-de <chen-de@aeonpeople.biz>
2019-08-26 10:40:04 +0800

    change user bean

--- src/main/java/com/cdh/git/testgit/pojo/User.java
+++ src/main/java/com/cdh/git/testgit/pojo/User.java
@@ -1,20 +1,10 @@
-/**
- * Copyright (C), 2015-2019, XXX有限公司
- * FileName: User
- * Author:   Admin
- * Date:     2019/8/26 10:21
- * Description: uu
- * History:
- * <author>          <time>          <version>          <desc>
- * 作者姓名           修改时间           版本号              描述
- */
+
 package com.cdh.git.testgit.pojo;
 
 import lombok.Data;
 
 /**
- * 〈一句话功能简述〉<br> 
- * 〈uu〉
+ * user <br>
  *
  * @author Admin
  * @create 2019/8/26
@@ -25,4 +15,5 @@ public class User {
     private String name;
     private String email;
     private int age;
+    private String address;
 }
