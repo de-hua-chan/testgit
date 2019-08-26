e1720f9dfe958fdec28540450ac19b2eac7b5f01
chen-de <chen-de@aeonpeople.biz>
2019-08-26 10:28:03 +0800

    add

---
+++ src/main/java/com/cdh/git/testgit/pojo/User.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: User
+ * Author:   Admin
+ * Date:     2019/8/26 10:21
+ * Description: uu
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.cdh.git.testgit.pojo;
+
+import lombok.Data;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈uu〉
+ *
+ * @author Admin
+ * @create 2019/8/26
+ * @since 1.0.0
+ */
+@Data
+public class User {
+    private String name;
+    private String email;
+    private int age;
+}
